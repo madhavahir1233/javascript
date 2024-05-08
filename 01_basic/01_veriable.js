10546c298cdd5e5c0458f26bb7755b3994327988
madhavahir1233 <168894003+madhavahir1233@users.noreply.github.com>
2024-05-08 06:59:37 +0000

    container config done

---
+++ 01_basic/01_veriable.js
@@ -0,0 +1,7 @@
+const accountId = 1223;
+let accountEmail = "madhav@gmail.com";
+var accointPassword = 2334;
+accountState="gujarat";
+
+
+console.table([accountId,accountEmail,accointPassword,accountState])
